f05f0c9792013d0559e29c40b9a69ffb1e4d34f9
av-sapkota <av.sapkota999@gmail.com>
2020-11-22 13:38:47 +0545

    22 Nov - MenuBar (with Menus and Sub-Menus) Created

--- GPLA_Assessment/Form1.Designer.cs
+++ GPLA_Assessment/Form1.Designer.cs
@@ -29,13 +29,90 @@ namespace GPLA_Assessment
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.userGuidelinesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.userGuidelinesToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(65, 29);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // userGuidelinesToolStripMenuItem
+            // 
+            this.userGuidelinesToolStripMenuItem.Name = "userGuidelinesToolStripMenuItem";
+            this.userGuidelinesToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.userGuidelinesToolStripMenuItem.Text = "User Guidelines";
+            this.userGuidelinesToolStripMenuItem.Click += new System.EventHandler(this.userGuidelinesToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem userGuidelinesToolStripMenuItem;
     }
 }
 
--- GPLA_Assessment/Form1.cs
+++ GPLA_Assessment/Form1.cs
@@ -16,5 +16,71 @@ namespace GPLA_Assessment
         {
             InitializeComponent();
         }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string message = "This application has been developed to produce a simplified programming " +
+                "language and environment.The application is an assignment designed by " +
+                "Leeds Beckett University for Level 6 Students who are studying " +
+                "computing. It covers the basics of sequence, selection and iteration " +
+                "and allows the developers to explore graphical developments in C# " +
+                "using Visual Studio Community 2019.\n\nApplication Programmer: " +
+                "\nAvhimanhu Sapkota\nUniversity ID: 77202323\nThe British College\n\n" +
+                "Copyright © 2020. All rights reserved.";
+
+            string title = "About - Graphical Programming Language Application";
+
+            MessageBox.Show(message, title);
+        }
+
+        private void userGuidelinesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string message = "Application Interface:\nCanvas: It is a display panel or drawing " +
+                "area where you will be able to view your outputs in accordance to the commands " +
+                "you type in Program Window and Command Line Window.\n\nProgram Window: A text " +
+                "area where you can type your programming codes which will allow you to perform " +
+                "various tasks. This window supports multiple line of codes. It only supports " +
+                "some command prompts listed below. If you fail to type commands correctly, " +
+                "you may encounter errors.\n\nCommands supported in Program Window:\n1. Position " +
+                "Pen (moveTo) - This command will change the position of your pen to the " +
+                "specified position in the canvas. In place of moveTo you are supposed to " +
+                "give x-axis and y-axis coordinates to place you pen's position. Example: " +
+                "Position Pen 50, 100 will change the position of your pen to x-axis: 50 and " +
+                "y-axis: 100\n\n2. Pen draw (drawTo) - This command will draw a line from the " +
+                "current position of the pen to the specified position in the canvas. " +
+                "In place of drawTo you are supposed to give x-axis and y-axis coordinates " +
+                "which will be the end point of your line.  Example: Position draw 50, 100 " +
+                "will draw a line from current pen position to x-axis: 50 and y-axis: 100\n\n" +
+                "3. Rectangle <width>, <height> - This command will draw a rectangle of specified " +
+                "width and height from the current pen position. Example: Rectangle 50, 100 will " +
+                "draw a rectangle of height = 100 and width = 50 from the current pen position." +
+                "\n\n4. Circle < radius > -This command will draw a circle of specified radius from " +
+                "the current pen position.Example: Circle 20 will draw a circle of radius = 20 from " +
+                "the current pen position.\n\n5. Triangle - This command will draw a triangle from " +
+                "the current pen position. \n\n6. Pen < color > -This command will specify the color " +
+                "of the pen. However, this command only supports limited colors, they are: red, " +
+                "blue, green, …… If you fail to specify colors from the list you may encounter " +
+                "errors.\n\n7. Fill < on / off > -This command will specify color fills for " +
+                "shapes(rectangle, circle and triangle).Example: Fill on will display the " +
+                "shape filled with the chosen color while Fill off will display the shape " +
+                "outlined with the chosen color but the inside area of the shape will be " +
+                "transparent.\n\nCommand Line Window: A text area where you are supposed " +
+                "to type basic command prompts which will help you perform operations within " +
+                "the application.This window supports single line of code.It only supports " +
+                "command prompts listed below.If you fail to type commands correctly, " +
+                "you may encounter errors. \n\nCommands supported in Command Line Window:" +
+                "\nClear - This command clears the drawing area.\nRun - This command will " +
+                "execute the codes written in Program Window\nReset - This command resets the " +
+                "initial position of pen to top left corner of the screen";
+
+            string title = "User Guidelines - Graphical Programming Language Application";
+
+            MessageBox.Show(message, title);
+        }
     }
 }
